b64371b70b35216b01dff78db8e7a1e868cd749d
Felipe Jorge <felipejorgeas@gmail.com>
2016-11-23 23:29:51 -0200

    Adicionado: diff de ingredientes; separate; rand; limit.

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -2,21 +2,39 @@ module.exports = function (utils, models) {
     var Recipe = {
         find: function (req, res) {
             utils.startTimeResponse();
-            var search = req.query.search.split(" ");
-            var where = {
-                $text: {
-                    $search: '\"' + search.join('\" \"') + '\"'
-                }
-            };
+            var search = req.query.search.replace(/  /g, ' ').split(" ");
+            var separate = req.query.separate;
+            var rand = req.query.rand;
+            var limit = req.query.limit;
+            if(separate){
+                var where = {
+                    $text: {
+                        $search: search.join(' ')
+                    }
+                };
+            } else {
+                var where = {
+                    $text: {
+                        $search: '\"' + search.join('\" \"') + '\"'
+                    }
+                };
+            }
+            var random = 0;
+            if(rand){
+                random = Math.floor(Math.random() * 2);
+            }
             var score = {
                 score: {
                     $meta: "textScore"
                 }
             };
             var sort = score;
-            models.Recipe.find(where, score).sort(sort).select('_id title image time').exec(function (err, result) {
+            models.Recipe.find(where, score).sort(sort).skip(random).limit(limit).select('_id title image time ingredients').exec(function (err, result) {
                 if (err) {
                     return res.status(500).send(utils.setResponseError(err));
+                }
+                if(result.length){
+                    result = Recipe.formatRecipesSearch(result, search);
                 }
                 return res.send(utils.setResponseSuccess(result));
             });
@@ -48,6 +66,34 @@ module.exports = function (utils, models) {
             } catch (ex) {
                 return res.status(400).send(utils.setResponseError({error: 'Requisição com dados inválidos'}));
             }
+        },
+        formatRecipesSearch: function(result, search){
+            function compare(a, b){
+                if (a.diff.length < b.diff.length){
+                    return -1;
+                }
+                if (a.diff.length > b.diff.length){
+                    return 1;
+                }
+                return 0;
+            }
+            var resultOk = [];
+            result.forEach(function(recipe){
+                var diff = [];
+                recipe.ingredients.forEach(function(list){
+                    diff = diff.concat(utils.diffItems(list.items, search));
+                });
+                var recipeOk = {
+                    _id: recipe._id,
+                    title: recipe.title,
+                    time: recipe.time,
+                    image: recipe.image,
+                    diff: diff
+                };
+                resultOk.push(recipeOk);
+            });
+            resultOk.sort(compare);
+            return resultOk;
         }
     };
     return Recipe;
